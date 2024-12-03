bdc9abcd41d0f41359c1bc03677070af27445977
Mikaël Dusenne <mikaeldusenne@gmail.com>
2024-12-03 10:11:43 +0100

    IO only in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,32 @@
+{-# LANGUAGE ScopedTypeVariables, LambdaCase #-}
 module Main (main) where
 
-import Lib
+import Lib (solution)
+import Control.Exception (try, IOException)
+import System.FilePath ((</>))
+import System.Environment (getEnv)
+import Data.List (intercalate)
+
+putSolution :: Show a => String -> a -> IO()
+putSolution p = putStrLn . (("Solution " ++ p ++ " : ")++) . show
+
+loadData :: Int -> IO String
+loadData n = (>>=readFile) $ (</>(show n++".csv")) <$> getEnv "ADVENTCALENDAR2024_DATA_DIR" 
+
+tupleToList (a, b) = [a, b]
+
+run :: Int -> IO String
+run n = try @IOException  (loadData n) >>= \case
+    Left _ -> return ("No data for problem " ++ show n)
+    Right s -> case solution n s of
+      Left err -> return err
+      Right result -> return $ init . unlines $ zipWith showResult "ab" (tupleToList result)
+        where showResult k val = let prettyprob = "problem " ++ show n ++ [k] ++ " : " in 
+                case val of (Just e) ->  prettyprob ++ show e
+                            Nothing ->  prettyprob ++ " not implemented yet"
+                
 
 main :: IO ()
 main = do
-  run 2
-  -- mapM_ run [1..25]
+  let sep = "\n────────────────────────────────\n"
+  mapM run [1..25] >>= (putStrLn . (sep++) .(++sep) . intercalate sep)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,58 +1,39 @@
-{-# LANGUAGE ScopedTypeVariables, LambdaCase #-}
-module Lib
-    ( run
-    ) where
+{-# LANGUAGE ScopedTypeVariables #-}
+module Lib (
+  solution
+  ) where
 
-import System.Environment (getEnv)
 import qualified Data.List as L
-import System.FilePath ((</>))
-import Control.Exception (try, IOException)
 import qualified Data.HashMap.Strict as H
 import SolutionHelpers (toTuple, quicksort, uniq, count, toDf, sign, oneoffs)
 
-
-putSolution :: Show a => String -> a -> IO()
-putSolution p = putStrLn . (("Solution " ++ p ++ " : ")++) . show
-
-loadData :: Int -> IO String
-loadData n = (>>=readFile) $ (</>(show n++".csv")) <$> getEnv "ADVENTCALENDAR2024_DATA_DIR" 
-
-run :: Int -> IO()
-run n = try @IOException  (loadData n) >>= \case
-    Left _ -> putStrLn ("No data for problem " ++ show n)
-    Right s -> solution n s
-
   
-solution :: Int -> String -> IO ()
-solution 1 s = do
-  let (la, lb) :: ([Int], [Int]) = unzip . map (toTuple . map (read :: String -> Int) . words) . lines $ s
-  let (sortedla, sortedlb) = (quicksort la, quicksort lb)
+solution :: Int -> String -> Either String (Maybe Int, Maybe Int)
+solution 1 s = Right (Just solution_a, Just solution_b)
+  where (la, lb) :: ([Int], [Int]) = unzip . map (toTuple . map (read :: String -> Int) . words) . lines $ s
+        (sortedla, sortedlb) = (quicksort la, quicksort lb)
+        solution_a = sum $ zipWith (\ a b -> abs (a - b)) sortedla sortedlb
       
-  putSolution "1a" $ sum $ zipWith (\ a b -> abs (a - b)) sortedla sortedlb
+        la1 = uniq sortedla
+        lb1 = uniq sortedlb
+        m = count $ filter (`elem` la1) sortedlb
+        solution_b = sum . map (\e -> (e * (m H.! e))) $ L.intersect la1 lb1
   
-  let la1 = uniq sortedla
-      lb1 = uniq sortedlb
-      m = count $ filter (`elem` la1) sortedlb
-      
-  putSolution "1b" $ sum . map (\e -> (e * (m H.! e))) $ L.intersect la1 lb1
-
+solution 2 s = Right (Just solution_a, Just solution_b)
   
-solution 2 s = do
-  
-  let df = toDf s
-      isOk :: [Int] -> Bool
-      isOk l = fst . foldr fok (True, 0) $ zipWith (-) (l) (tail l)
-        where fok e (False, sgn) = (False, sgn)
-              fok e (True, sgn) | (sgn == 0 || (sign e == sgn)) && abs e >= 1 && abs e <= 3 = (True, sign e)
-                                | otherwise = (False, sgn)
+  where df = toDf s
+        isOk :: [Int] -> Bool
+        isOk l = fst . foldr fok (True, 0) $ zipWith (-) (l) (tail l)
+          where fok e (False, sgn) = (False, sgn)
+                fok e (True, sgn) | (sgn == 0 || (sign e == sgn)) && abs e >= 1 && abs e <= 3 = (True, sign e)
+                                  | otherwise = (False, sgn)
                                 
-  putSolution "2a" $ length . filter id . map isOk $ df
+        solution_a = length . filter id . map isOk $ df
 
-  let isOk' :: [Int] -> Bool
-      isOk' = any isOk . oneoffs
+        isOk' :: [Int] -> Bool
+        isOk' = any isOk . oneoffs
 
-  putSolution "2b" $ length . filter id . map isOk' $ df
+        solution_b = length . filter id . map isOk' $ df
 
   
-  
-solution n _ = putStrLn $ "Problem " ++ show n ++ " not yet implemented !"
+solution n _ = Left $ "Problem " ++ show n ++ " not yet implemented !"
